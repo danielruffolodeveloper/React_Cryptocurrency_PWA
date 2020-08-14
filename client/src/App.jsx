97afec33f764f678bc5e89c814d4ce466c5852a6
Daniel Ruffolo <danielruffolodeveloper@gmail.com>
2020-08-15 07:27:24 +1000

    Added Bootstrap , custom styling, Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import CoinDetailPage from './pages/CoinDetailPage';
 import CoinSummaryPage from './pages/CoinSummaryPage';
+
+import Header from './components/Header';
+
+
 import {BrowserRouter, Route} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <BrowserRouter>
         <Route exact path="/" component={CoinSummaryPage}/>
       </BrowserRouter>
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-center  mt-3 mb-4">Crypto PWA</h1>
+        </div>
+    )
+}
+
+export default Header

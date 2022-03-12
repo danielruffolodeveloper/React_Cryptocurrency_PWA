c1efd7af1655ac813cab7a30f11ffb06fa7b6f0e
Daniel Ruffolo <danielruffolodeveloper@gmail.com>
2022-03-12 12:52:03 +1100

    updgrate to MUI and MUI list for coin data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,21 +5,28 @@ import CoinDetailPage from './pages/CoinDetailPage';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { WatchListContextProvider } from "./context/WatchListContext";
-import {BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
 import './App.css';
 
 function App() {
   return (
-  <div className="container-fluid">
+    <div className="container-fluid">
 
-    <WatchListContextProvider>
+      <WatchListContextProvider>
         <BrowserRouter>
-        <Navbar/>
-          <Route exact path="/" component={CoinSummaryPage}/>
-          <Route exact path="/coins/:id" component={CoinDetailPage}/>
-
+          <CssBaseline />
+          <Container maxWidth="sm">
+            <Navbar />
+            <Route exact path="/" component={CoinSummaryPage} />
+            <Route exact path="/coins/:id" component={CoinDetailPage} />
+          </Container>
         </BrowserRouter>
-        <Footer/>
+        <Footer />
       </WatchListContextProvider>
     </div>
   );
--- client/src/components/CoinList.jsx
+++ client/src/components/CoinList.jsx
@@ -1,60 +1,84 @@
 import React, { useEffect, useState, useContext } from "react";
 import coinGecko from "../api/coinGecko";
 import { WatchListContext } from "../context/WatchListContext";
-import Coin from "./Coin";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
 
 const CoinList = () => {
+  const [coins, setCoins] = useState([]);
+  const { watchList, deleteCoin } = useContext(WatchListContext);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [coins, setCoins] = useState([]);
-    const { watchList, deleteCoin } = useContext(WatchListContext);
-    const [isLoading, setIsLoading] = useState(false);
-
-    useEffect(() => {
-
-        const fetchData = async () => {
-            setIsLoading(true);
-
-            const response = await coinGecko.get("coins/markets", {
-                params: {
-                    vs_currency: "aud",
-                    ids: watchList.join(",")
-                }
-            })
-
-            setCoins(response.data);
-            setIsLoading(false);
-
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const response = await coinGecko.get("coins/markets", {
+        params: {
+          vs_currency: "aud",
+          ids: watchList.join(",")
         }
-        
-        if (watchList.length > 0) {
-            fetchData();
-          } else setCoins([]);
-        }, [watchList]);
+      })
+      setCoins(response.data);
+      setIsLoading(false);
+    }
 
-    
+    if (watchList.length > 0) {
+      fetchData();
+    } else setCoins([]);
+  }, [watchList]);
 
-    const renderCoins = () => {
-        if (isLoading) {
-          return <div>Loading...</div>;
-        }
-    
-        return (
-          // <ul className="coinlist list-group mt-2">
-        
-          // </ul>
+  const renderCoinList = () => {
+    console.log(coins)
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
 
-          <div className="row">
-            {coins.map((coin) => {
-                return <Coin key={coin.id} coin={coin} deleteCoin={deleteCoin} />;
-            })}
-          </div>
-        );
-      };
-    
-      return <div>{renderCoins()}</div>;
-    };
+    return (
+      <List dense={true} sx={{ width: '100%', bgcolor: 'background.paper' }}>
+        {coins.map((coin) => (
+          <>
+            <ListItem alignItems="flex-center"
+              secondaryAction={
+                <ListItemText
+                  primary={
 
+                    <Box sx={{ display: 'flex', alignItems: 'left', textAlign:'right' }}>
+                      <Box sx={{ fontSize: '0.875rem' }}>
+                        <div>
+                          {coin.current_price.toLocaleString('en-AU', { style: 'currency', currency: 'AUD' })}
 
+                        </div>
+                        <div>
+                          {coin.market_cap > 1000000000000 ?
+                            (coin.market_cap / 1000000000000).toFixed(2) + "T" :
+                            coin.market_cap > 1000000 ?
+                              (coin.market_cap / 1000000).toFixed(2) + "M" :
+                              coin.market_cap.toLocaleString('en-AU', { style: 'currency', currency: 'AUD' })
+                          }
+                        </div>
+                      </Box>
+                    </Box>
+                  } />
+              }>
 
-
+              <ListItemAvatar>
+                <Avatar alt={coin.name} src={coin.image} />
+              </ListItemAvatar>
+              <ListItemText
+                primary={coin.name}
+              />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+          </>
+        ))}
+      </List>
+    );
+  };
+  return <div>{renderCoinList()}</div>;
+};
 export default CoinList
